b1f66bafdcd683f62e71a550117b534578b712e4
MorteritoKeloke <zoroarkb@gmail.com>
2022-12-13 22:31:02 +0100

    dsa
    asd

--- Assets/Scripts/coins.cs
+++ Assets/Scripts/coins.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class coins : MonoBehaviour
 {
+    //I used this script, to give the coins a visual touch
+    //I have put the rotation speed of the coins here
     private float speed = 180;
 
     void Update()
     {
+        //here we tell him to rotate on himself with x
         transform.Rotate(speed * Time.deltaTime, 0, 0);
     }
 }
